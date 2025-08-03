0275279b2fa348e9e2e5c040f6f4d2e958d243a0
inevitablegs <sonawaneganu3101@gmail.com>
2025-08-03 23:45:56 +0530

    Allowed origin updated

--- imageupscaleai/settings.py
+++ imageupscaleai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k-#(g#zro^!ek5ipg195i1$gqpqkzznp+nv3a0q8zoz8qy6u&o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
